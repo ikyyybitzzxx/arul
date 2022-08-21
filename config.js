49a429f83c41aad82f98c1aeec9bd7b7e46205d5
ikyyybitzzxx <106673608+ikyyybitzzxx@users.noreply.github.com>
2022-08-21 14:52:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -125,10 +125,10 @@ global.author = global.wm
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://telegra.ph/file/c73f47b440241b66cb9d3.jpg' //Main Thumbnail
-global.thumb2 = 'https://telegra.ph/file/c73f47b440241b66cb9d3.jpg'
-global.thumbbc = 'https://telegra.ph/file/d389f4acafac741f6592c.jpg' //For broadcast
-global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
+global.thumb = 'https://telegra.ph/file/99b894d82b3de1ce07993.jpg' //Main Thumbnail
+global.thumb2 = 'https://telegra.ph/file/99b894d82b3de1ce07993.jpg'
+global.thumbbc = 'https://telegra.ph/file/99b894d82b3de1ce07993.jpg' //For broadcast
+global.giflogo = 'https://telegra.ph/file/99b894d82b3de1ce07993.jpg'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
@@ -143,7 +143,7 @@ global.dxlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.shee
 global.dpdf = 'application/pdf'
 global.drtf = 'text/rtf'
 
-global.thumbdoc = 'https://telegra.ph/file/6e45318d7c76f57e4a8bd.jpg'
+global.thumbdoc = 'https://telegra.ph/file/99b894d82b3de1ce07993.jpg'
 
 /*=========== FAKE SIZE ===========*/
 global.fsizedoc = '99999999999999' // default 10TB
